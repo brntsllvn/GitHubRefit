8e4764a7211c19ff0f759fed8851bf9e1eb7bad4
Brent Sullivan <brntsllvn@gmail.com>
2016-02-16 12:22:48 -0800

    wrap github api

---
+++ GithubRefit/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using GithubRefit.Models;
+using Refit;
+
+namespace GithubRefit.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+
+            var gitHubApi = RestService.For<IGitHubApi>("https://api.github.com");
+
+            var request = new RenderRequest
+            {
+                Text = "Hello **world**, this is sample markdown [with a URL](http://www.somewhere.com)",
+                Mode = "markdown"
+            };
+
+            var response = await gitHubApi.RenderAsMarkdown(request);
+
+            ViewBag.Markdown = request.Text;
+            ViewBag.Html = response;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ GithubRefit/Models/IGitHubApi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace GithubRefit.Models
+{
+    [Headers("User-Agent: Refit Demo App")]
+    public interface IGitHubApi
+    {
+        [Post("/markdown")]
+        Task<string> RenderAsMarkdown([Body] RenderRequest request);
+    }
+}
